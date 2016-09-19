33efe743e4e1aa71b99c1a4b0123fad9d884fc32
Greg Kent <gregokent@gmail.com>
2016-09-18 22:17:59 -0400

    started replicating svstat functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let dirs = args.iter().skip(1);
+    let currpath = Path::new(".");
+    let origdir = match File::open(&currpath) {
+        Err(why) => panic!("couldn't open current directory: {}",why.description()),
+        Ok(origdir) => origdir,
+    };
+
+    for dir in dirs {
+        println!("{:?}", dir );
+    }
     println!("Hello, world!");
 }
